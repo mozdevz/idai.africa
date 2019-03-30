cbb16814835b2fe5ace9207632e2be899b9dec5f
_celso <celso.r.paunde@gmail.com>
2019-03-30 17:39:52 +0200

    Merged with master

---
+++ app/Http/Controllers/VolunteerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+use App\Models\Volunteer\Volunteer;
+
+class VolunteerController extends Controller
+{
+
+	public function addVolunteer(User $user){
+		
+	}
+    
+}
