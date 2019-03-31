6906614e117a1abf955240a738e39ea3af0c1bbc
Calvin Carlos da Conceição Chiulele <cchiulele@protonmail.com>
2019-03-31 08:51:59 +0200

    Travis conf file added (#8)
    * Travis conf file added
    
    * config/app.php file changed run php artisan migrate with no interaction
    
    * script block commented on travis.yml
    
    * tests files deleted @ /tests and some changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,25 +31,6 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{__('Victims')}} <span class="caret"></span>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{url('/')}}/person/new">{{__('Report a Missing Person')}}</a>
-                                <a class="dropdown-item" href="{{url('/')}}/person/new">{{__('Report People in Need of Help')}}</a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/')}}/person/new }}">{{ __('Volunteers') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/')}}/person/new }}">{{ __('Donations') }}</a>
-                        </li>
-                    </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
