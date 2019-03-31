f2019ca5ce267e74a0d617397708f2bdbbcf9b87
Calvin Carlos da Conceição Chiulele <cchiulele@protonmail.com>
2019-03-31 16:17:20 +0200

    Feita meia parte do módulo das organizações (#9)

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Repositories\Providers\OrganizationProvider::class,
 
     ],
 
