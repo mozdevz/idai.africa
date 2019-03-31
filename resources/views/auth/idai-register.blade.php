9981f5640e8fde8b57ab007dfccee143b5833e9c
Jose Seie <joseeugenioseie@hotmail.com>
2019-03-31 15:53:21 +0200

    Volunteer part01 registration done.

--- resources/views/auth/idai-register.blade.php
+++ resources/views/auth/idai-register.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
     <link rel="shortcut icon" href="assets/images/logo-final2-1-122x86.png" type="image/x-icon">
     <meta name="description" content="">
-    <title>login | Volunteers</title>
+    <title>Register | Volunteers</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 
     <style type="text/css">
@@ -39,15 +39,26 @@
 
 <div class="form-box">
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('register') }}">
 
         @csrf
 
-        <h1 class="text-center">{{ __('Login') }}</h1>
+        <h1 class="text-center">{{ __('Register') }}</h1>
+
+        <div class="form-group">
+            <label for="name"><h2>{{ __('Name') }}</h2></label><br>
+            <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+
+            @if ($errors->has('name'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('name') }}</strong>
+                </span>
+            @endif
+        </div>
 
         <div class="form-group">
             <label for="email"><h2>{{ __('Email') }}</h2></label><br>
-            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
             @if ($errors->has('email'))
                 <span class="invalid-feedback" role="alert">
@@ -58,7 +69,7 @@
 
         <div class="form-group">
             <label for="password"><h2>{{ __('Password') }}</h2></label><br>
-            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" value="{{ old('password') }}" required><br>
+            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" value="{{ old('password') }}" required>
 
             @if ($errors->has('password'))
                 <span class="invalid-feedback" role="alert">
@@ -67,9 +78,14 @@
             @endif
         </div>
 
+        <div class="form-group">
+            <label for="password-confirm"><h2>{{ __('Confirm Password') }}</h2></label><br>
+            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required><br>
+        </div>
+
         <input type="submit" name="submit" class="btn btn-primary btn-block">
 
-        <div>You are not yet a vonlunteer? <a href="/register">register here</a></div>
+        <div>You are yet a vonlunteer? <a href="/login">Login here</a></div>
     </form>
 </div>
 
